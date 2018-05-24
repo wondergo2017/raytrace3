2621be13ea4cef7311782cb85d62ece252b418fc
wondergo2017 <357429727@qq.com>
2018-05-24 14:37:36 +0800

    failure

--- raytrace3/raytrace3/main.cpp
+++ raytrace3/raytrace3/main.cpp
@@ -8,10 +8,14 @@ inline bool IsZero(double i) { return abs(i) < M_EPS; }
 #define Color Vec3
 #define DEBUG
 #define MAX_NUM 1e20
+#define MIN_NUM -1e20
 #define PI 3.1415926
 #define PI_INV 0.3183098862
 #define MIN_WGT 1e-3
 #define MAX_DEP 20
+#define INIT_R2 20
+#define MAX_VEC3 (Vec3(MAX_NUM,MAX_NUM,MAX_NUM))
+#define MIN_VEC3 (Vec3(MIN_NUM,MIN_NUM,MIN_NUM))
 cv::RNG rng;
 const double alpha = 0.7;//光子数量更新因子
 double GetRandu(double r1, double r2)
@@ -98,14 +102,14 @@ public:
 	{
 		return Ray(pos, Vec3(PicOrg + du * (x - hh) + dv * (y - hw)).GetUnit());
 	}
-	void init(int h_ = 500, int w_ = 500, int fovAng = 45, const Vec3 &pos_ = Vec3(25, 58, 210.6), const Vec3&dir_ = Vec3(0, -0.122612, -1)) {
+	void init(int h_ = 500, int w_ = 500, int fovAng = 45, const Vec3 &pos_ = Vec3(25, 58, 200.6), const Vec3&dir_ = Vec3(0, -0.122612, -1)) {
 #ifdef DEBUG
 		std::cout << "camera::init()!" << std::endl;
 #endif // DEBUG
 		dir = dir_;
 		pos = pos_;
 		
-		sr = 1.0 / 1;
+		sr = 1.0 / 100;
 		fov_h = fovAng * acos(-1.0) / 180;
 
 		h = h_;
@@ -162,6 +166,7 @@ public:
 		dist = d;
 		obj = o;
 	}
+	bool inside = 0;
 };
 enum OBJ_REL { INSIDE, OUTSIDE, ON, UNKNOWN };//和物体的相对位置
 class Obj
@@ -210,11 +215,11 @@ class PlaneObj :public Obj
 {
 public:
 	Vec3 P0;//平面上一点
-	Vec3 n;//单位法向量
-	PlaneObj(const Vec3& pp, const Vec3 &nn, const Material & mm, const Color &cc = Color(1, 1, 1)) :P0(pp), n(nn), Obj(mm, cc) {}
+	Vec3 norm;//单位法向量
+	PlaneObj(const Vec3& pp, const Vec3 &nn, const Material & mm, const Color &cc = Color(1, 1, 1)) :P0(pp), norm(nn), Obj(mm, cc) {}
 
 	Collision GetIntersect(const Ray & ray) override {
-		double t = -(n.dot(P0) + n.dot(ray.org)) / (n.dot(ray.dir));
+		/*double t = -(n.dot(P0) + n.dot(ray.org)) / (n.dot(ray.dir));
 		Collision ctemp;
 		if (t > 0)
 		{
@@ -223,15 +228,27 @@ public:
 			ctemp.From = ray.dir;
 			ctemp.N = GetNormal(ctemp.Pos);
 			ctemp.obj = this;
-		}
+		}*/
+		Collision ctemp; 
+		Vec3 op(ray.org - P0), n;
+		if (IsZero(norm.dot(ray.dir))) return ctemp;
+		ctemp.From = ray.dir;
+		if (!IsZero(norm.dot(op))) { ctemp.N = norm; ctemp.inside = false; }
+		else { ctemp.N = norm * -1.; ctemp.inside = true; }
+		double dis =ctemp.N.dot(op);
+		double project = ctemp.N.dot(ray.dir);
+		if (!IsZero(project)) return ctemp;
+		ctemp.dist = dis / (-project);
+		ctemp.Pos = ray.org + ray.dir * ctemp.dist;
+		ctemp.obj = this;
 		return ctemp;
 	}
 	Vec3 GetNormal(const Vec3 & v) {
-		return n;
+		return norm;
 	}
 	double GetDist(const Vec3 & pos, const Vec3 & vec)
 	{
-		return n.dot(pos)*n.dot(vec);
+		return norm.dot(pos)*norm.dot(vec);
 	}
 };
 class SphereObj :public Obj
@@ -241,60 +258,77 @@ public:
 	double radius;
 	SphereObj(double rradius, const  Vec3& orgg, const Material& mm, const Color&cc = Color(1, 1, 1)) :org(orgg), radius(rradius), Obj(mm, cc) {}
 	virtual Collision GetIntersect(const Ray & ray) override {
-		//std::cout <<"vec:"<<vec;
+//		//std::cout <<"vec:"<<vec;
+//		Collision ctemp;
+//		double r2 = radius * radius;
+//		Vec3 l = org - ray.org;//光源和球中心的向量
+//		double tp = l.dot(ray.dir);//垂足的t
+//		OBJ_REL flag = UNKNOWN;//光源与物体相对位置
+//							   //判断光源与物体相对位置
+//		if (l.Module2() < r2)
+//		{
+//			flag = INSIDE;
+//		}
+//		else if (l.Module2() > r2)
+//		{
+//			flag = OUTSIDE;
+//		}
+//		else {
+//			flag = ON;
+//		}
+//		//光线与球面不相交
+//		if (tp < 0 && flag == OUTSIDE)
+//		{
+//#ifdef DEBUG
+//			//std::cout << "OUTSIDE" << std::endl;
+//#endif // DEBUG
+//			return ctemp;
+//		}
+//		double d2 = l.Module2() - tp * tp;// 球心到光线所在直线的距离的平方
+//		if (d2 > r2)//光线与球面不相交
+//		{
+//#ifdef DEBUG
+//			//std::cout << "d2>r2" << std::endl;
+//#endif // DEBUG
+//			return ctemp;
+//		}
+//		double t2 = r2 - d2;//投影点到光线与球面的交点距离的平方
+//		double t = 0;// 最终直线方向参量
+//		if (flag == OUTSIDE)
+//		{
+//			t = tp - sqrt(t2);
+//		}
+//		else if (flag == INSIDE)
+//		{
+//			t = tp + sqrt(t2);
+//		}
+//		else {//在球面上，待处理
+//#ifdef DEBUG
+//			std::cout << "on the Sphere!" << std::endl;
+//#endif // DEBUG
+//		}
+//		ctemp.Pos = ray.org + ray.dir*t;
+//		ctemp.dist = GetDist(ray.org, ray.dir);
+//		ctemp.From = ray.dir;
+//		ctemp.N = GetNormal(ctemp.Pos);
+//		ctemp.obj = this;
 		Collision ctemp;
-		double r2 = radius * radius;
-		Vec3 l = org - ray.org;//光源和球中心的向量
-		double tp = l.dot(ray.dir);//垂足的t
-		OBJ_REL flag = UNKNOWN;//光源与物体相对位置
-							   //判断光源与物体相对位置
-		if (l.Module2() < r2)
+		Vec3 op = org - ray.org;
+		if (IsZero(op.dot(op) - radius*radius)) ctemp.inside = true;
+		else ctemp.inside = false;
+		double t, b = op.dot( ray.dir), det = b * b - op.dot( op) + radius * radius;
+		if (det < 0) { ctemp.dist = MAX_NUM; }
+		else
 		{
-			flag = INSIDE;
+			det = sqrt(det);
+			ctemp.dist = (t = b - det) > 1e-8 ? t : ((t = b + det) > 1e-8 ? t : MAX_NUM);
+			ctemp.Pos = ray.org + ray.dir * t;
+			ctemp.N = Vec3(ctemp.Pos - org).GetUnit();
+			ctemp.inside = ctemp.N.dot( ray.dir) > 0;
+			ctemp.N = ctemp.inside ? ctemp.N * -1 : ctemp.N;
+			ctemp.From = ray.dir;
+			ctemp.obj = this;
 		}
-		else if (l.Module2() > r2)
-		{
-			flag = OUTSIDE;
-		}
-		else {
-			flag = ON;
-		}
-		//光线与球面不相交
-		if (tp < 0 && flag == OUTSIDE)
-		{
-#ifdef DEBUG
-			//std::cout << "OUTSIDE" << std::endl;
-#endif // DEBUG
-			return ctemp;
-		}
-		double d2 = l.Module2() - tp * tp;// 球心到光线所在直线的距离的平方
-		if (d2 > r2)//光线与球面不相交
-		{
-#ifdef DEBUG
-			//std::cout << "d2>r2" << std::endl;
-#endif // DEBUG
-			return ctemp;
-		}
-		double t2 = r2 - d2;//投影点到光线与球面的交点距离的平方
-		double t = 0;// 最终直线方向参量
-		if (flag == OUTSIDE)
-		{
-			t = tp - sqrt(t2);
-		}
-		else if (flag == INSIDE)
-		{
-			t = tp + sqrt(t2);
-		}
-		else {//在球面上，待处理
-#ifdef DEBUG
-			std::cout << "on the Sphere!" << std::endl;
-#endif // DEBUG
-		}
-		ctemp.Pos = ray.org + ray.dir*t;
-		ctemp.dist = GetDist(ray.org, ray.dir);
-		ctemp.From = ray.dir;
-		ctemp.N = GetNormal(ctemp.Pos);
-		ctemp.obj = this;
 		return ctemp;
 	}
 	Vec3 GetNormal(const Vec3& v1)//得到法向量(没有单位化)
@@ -420,7 +454,13 @@ struct HitPoint//视点碰撞点
 		out << "pos:" << hp.pos << "x,y:" << hp.x << " " << hp.y << endl << "R2:" << hp.R2 << " tao" << hp.tao << endl;
 		return out;
 	}
-	explicit HitPoint(const Vec3& pos_ = Vec3(), const Vec3& n_ = Vec3(), const Vec3& v_ = Vec3(), Obj* o = nullptr,int xx=-1,int yy=-1,const Color& wgt_=Color()):pos(pos_),n(n_),v(v_),obj(o),x(xx),y(yy),wgt(wgt_){}
+	explicit HitPoint(const Vec3& pos_ = Vec3(), const Vec3& n_ = Vec3(), const Vec3& v_ = Vec3(), Obj* o = nullptr,int xx=-1,int yy=-1,const Color& wgt_=Color()):pos(pos_),n(n_),v(v_),obj(o),x(xx),y(yy),wgt(wgt_){
+		R2 = INIT_R2;
+		N = 0;
+		tao = Color();
+
+	}
+	
 };
 inline void putxy(int x, int y, int chan, uchar color, cv::Mat& m) {
 	m.ptr<uchar>(y)[3 * x + chan] = color;
@@ -432,6 +472,70 @@ inline void putxy(int x, int y, const Color& c, cv::Mat& m)
 		putxy(x, y, 0, c.val[i], m);
 	}
 }
+//
+bool LtX(HitPoint* a, HitPoint* b) { return a->pos[0] < b->pos[0]; }
+bool LtY(HitPoint* a, HitPoint* b) { return a->pos[1] < b->pos[1]; }
+bool LtZ(HitPoint* a, HitPoint* b) { return a->pos[2] < b->pos[2]; }
+class KdNode
+{
+public:
+	KdNode(HitPoint* hp=nullptr,KdNode* lc_=nullptr,KdNode *rc_=nullptr,int dim_=0,const Vec3 &min_=MAX_VEC3,const Vec3 &max_=MIN_VEC3) :data(hp) {
+		data = hp;
+		lc = lc_;
+		rc = rc_;
+		dim = dim_;
+		min = min_;
+		max = max_;
+	}
+	HitPoint *data;
+	KdNode *lc, *rc;
+	int dim;
+	Vec3 min, max; // 包围盒
+};
+
+class KdTree
+{
+public:
+	void build(vector<HitPoint*> hps)
+	{
+		root = build(hps, 0);
+	}
+	void update(const Collision &c, const Color &flux) {
+		update(root,c, flux);
+	}
+	KdNode* root;
+	KdNode* build(vector<HitPoint*> hps, int dim)
+	{
+		//cout << "build:" << dim << endl;
+		if (hps.size() < 1) return NULL;
+		if (hps.size() < 2) return new KdNode(hps[0]);
+
+		if (dim == 0) sort(hps.begin(), hps.end(), LtX);
+		else if (dim == 1) sort(hps.begin(), hps.end(), LtY);
+		else sort(hps.begin(), hps.end(), LtZ);
+
+		int mid = hps.size() >> 1;
+		vector<HitPoint*> lhp, rhp;
+		for (int i = 0; i < mid; i++)
+			lhp.push_back(hps[i]);
+		for (int i = mid + 1; i < hps.size(); i++)
+			rhp.push_back(hps[i]);
+		return new KdNode(hps[mid], build(lhp, (dim + 1) % 3), build(rhp, (dim + 1) % 3), dim);
+	};
+	void update(KdNode* r, const Collision &c, const Color &flux)
+	{
+		if (!r) return;
+		if (dist2(r->data->pos, c.Pos) <= r->data->R2 && !IsZero(r->data->n.dot(c.N)))
+		{
+			double g = (r->data->N * alpha + alpha) / (r->data->N * alpha + 1.0);
+			r->data->R2 = r->data->R2 * g;
+			r->data->N++;
+			r->data->tao = (r->data->tao + r->data->wgt.mul(flux) * (PI_INV)) * g;
+		}
+		if (c.Pos[r->dim] >= r->data->pos[r->dim] - sqrt(r->data->R2)) update(r->rc, c, flux);
+		if (c.Pos[r->dim] <= r->data->pos[r->dim] + sqrt(r->data->R2)) update(r->lc, c, flux);
+	}
+};
 
 
 class RayTracer
@@ -441,6 +545,7 @@ public:
 	Camera camera;
 	Scene scene;
 	vector<HitPoint*> Hvec;//用来保存视点碰撞点
+	KdTree kdtree;
 	void init()
 	{
 #ifdef DEBUG
@@ -463,7 +568,26 @@ public:
 			}
 		}
 		//debug
-		//cout << "hitpoint::" << Hvec.size() << endl;
+		cout << "hitpoint::" << Hvec.size() << endl;
+	}
+	void UpdateB(const Collision&c, const Color& flux)
+	{
+		//brute force
+		for (auto it=Hvec.begin();it!=Hvec.end();it++)
+		{
+			if (dist2((**it).pos, c.Pos) < (**it).R2)
+			{
+				//Update
+				double g = ((*it)->N * alpha + alpha) / ((*it)->N * alpha + 1.0);
+				(*it)->R2 = (*it)->R2 * g;
+				(*it)->N++;
+				(*it)->tao = ((*it)->tao + (*it)->wgt.mul( flux) * (PI_INV)) * g;
+			}
+		}
+	}
+	void UpdateK(const Collision&c, const Color& flux)
+	{
+		kdtree.update(c, flux);
 	}
 	void PhotonTrace(const Ray &ray, int dep, Color flux, Color wgt)
 	{
@@ -482,7 +606,7 @@ public:
 				obj->GetDiff(diff);
 				Vec3 diff_flux = flux.mul(diff);
 				//update
-
+				UpdateK(c1,flux);
 				//Roulette
 				if (Obj::React::DIFF==obj->Roulette())//go on diff
 				{
@@ -506,12 +630,14 @@ public:
 	}
 	void PhotonMap(int cnt)
 	{
+		cout << "PhotonMap:" << cnt << endl;
 		Ray ray;
 		Color flux;
 		for (auto pl:scene.plvec)
 		{
 			for (int i = 0; i < cnt; i++)
 			{
+				cout << i << endl;
 				pl->emit(ray, flux);
 				PhotonTrace(ray, 0, flux, Color(1, 1, 1));
 			}
@@ -563,6 +689,7 @@ public:
 	}
 	void GetColor()//计算tao得到L并且得到最终camera的color
 	{
+		cout << "GetColor()" << endl;
 		for (int i = 0; i < camera.h; i++)
 		{
 			for (int j = 0; j < camera.w; j++)
@@ -575,7 +702,8 @@ public:
 			//if (hp.tao.b != 0) cout << hp.x << " " << hp.y << " " << hp.tao.b << endl;
 			
 			//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!need to do
-			camera.pic[hp->x][hp->y] += hp->obj->color.mul(hp->tao*PI_INV / hp->R2);
+			//camera.pic[hp->x][hp->y] += hp->obj->color.mul(hp->tao*PI_INV / hp->R2);
+			camera.pic[hp->x][hp->y] += hp->tao* (PI_INV /(hp->R2*1000));
 		}
 		double maxnum = 0;
 		for (int i = 0; i < camera.h; i++)
@@ -600,10 +728,15 @@ public:
 	void render()
 	{
 		GetHitPoint();
+		BuildTree();
 		//debugHitPoint();
+		PhotonMap(3000);
+		GetColor();
+		show();
 	}
 	void show()
 	{
+		cout << "show" << endl;
 		cv::Mat* m1 = new cv::Mat(camera.h, camera.w, CV_8UC3);
 		for (int i = 0; i < camera.h; i++)
 		{
@@ -617,7 +750,12 @@ public:
 		}
 		cv::namedWindow("test");
 		cv::imshow("test", *m1);
-		cvWaitKey(1000);
+		cvWaitKey(0);
+	}
+	void BuildTree()
+	{
+		cout << "Buildtree" << endl;
+		kdtree.build(Hvec);
 	}
 };
 int main()
